b7ab64e3008322fe1da0649efbea8c0ff422cb56
titouan.fuchs <titouan.fuchs@uha.fr>
2021-09-10 16:15:29 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+    session_start();
+
+    $albums_data = file_get_contents('https://filrouge.uha4point0.fr/music/albums');
+    $albums =json_decode($albums_data,true);
+
+    $group_data = file_get_contents('https://filrouge.uha4point0.fr/music/groupes');
+    $groups = json_decode($group_data,true);
+
+    function returnGroup($groups, $id){
+        foreach ($groups as $group){
+            if ($group['id'] == $id){
+                return $group;
+            }
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>MusicPass</title>
+    <link rel="stylesheet" href="style/style.css"/>
+</head>
+
+<body>
+<header class="mainHeader">
+    <section class="logoSection">
+        <img title="je ne suis présent que pour combler un trou béant" style="width: 50%" src="https://www.avantjetaisriche.com/wp-content/uploads/2015/06/monsieur-mr-patate-homer-simpson.jpg"/>
+    </section>
+
+    <section class="researchSection">
+        <form method="POST" name="MusicSearch">
+            <fieldset class="transparentFieldSet">
+                <input type="text" name="researchArg" class="researchInput" placeholder="Recherche (Titre, Artiste, Album, Genre, etc...)"/>
+                <input type="submit" value="Rechercher" class="researchSubmit"/>
+            </fieldset>
+            <fieldset class="transparentFieldSet">
+                <input type="submit" value="Proposez-moi quelque chose de nouveau !" class="randomSubmit" title="Choisis aléatoirement une musique, ou un album !"/>
+            </fieldset>
+        </form>
+    </section>
+
+    <section class="userSection">
+        <img title="je ne suis présent que pour combler un trou béant" style="width: 50%" src="https://www.avantjetaisriche.com/wp-content/uploads/2015/06/monsieur-mr-patate-homer-simpson.jpg"/>
+    </section>
+</header>
+
+<section id="mainSection" class="mainSection">
+    <section class="contentHeader">
+
+    </section>
+    <section id="researchContent" class="researchContent">
+        <!--echo "<div class='musicCard'>
+            <section class='musicCard-pochette-Section'>
+                <img class='musicCard-pochette' src='images/NoCover.png'/>
+            </section>
+
+            <section class='musicCard-Info-Section'>
+                <h2>Album Title</h2>
+                <h3>Artist Name</h3>
+                <h4>Sortie</h4>
+                <h4>Pistes</h4>
+            </section>
+
+            <section class='musicCard-Action-Section'>
+                <button>Je l'ai écouté !</button>
+                <button>Favoris</button>
+            </section>
+        </div>";-->
+        <?php
+            foreach($albums as $album){
+                $album_pochette_url = $album['couverture'];
+                $album_name = $album['nom'];
+                $album_sortie = $album['sortie'];
+                $album_pistes = $album['pistes'];
+                $album_group_index = $album['artiste'];
+                $album_group = returnGroup($groups ,$album_group_index);
+                $album_group_name = $album_group['nom'];
+
+                echo "<div class='musicCard'>
+                        <section class='musicCard-pochette-Section'>
+                            <img class='musicCard-pochette' src='$album_pochette_url'/>
+                        </section>
+            
+                        <section class='musicCard-Info-Section'>
+                            <h2>$album_name</h2>
+                            <h3>$album_group_name</h3>
+                            <h4>$album_sortie</h4>
+                            <h4>$album_pistes</h4>
+                        </section>
+            
+                        <section class='musicCard-Action-Section'>
+                            <button>Je l'ai écouté !</button>
+                            <button>Favoris</button>
+                        </section>
+                    </div>";
+            }
+        ?>
+    </section>
+</section>
+<footer>
+    <img title="je ne suis présent que pour combler un trou béant" style="width: 15%; position: relative;left: 50%;" src="https://www.avantjetaisriche.com/wp-content/uploads/2015/06/monsieur-mr-patate-homer-simpson.jpg"/>
+</footer>
+</body>
+</html>
