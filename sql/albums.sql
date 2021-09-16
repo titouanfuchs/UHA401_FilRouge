9a49111913e8be1a4af25f06ce7837caf3fa0559
titouan.fuchs <titouan.fuchs@uha.fr>
2021-09-16 19:49:03 +0000

    Add Albums SQL

---
+++ sql/albums.sql
@@ -0,0 +1,17 @@
+CREATE TABLE albums(
+   nom        VARCHAR(30)
+  ,id         INTEGER 
+  ,artiste    INTEGER 
+  ,pistes     INTEGER 
+  ,sortie     INTEGER 
+  ,couverture VARCHAR(30)
+);
+INSERT INTO albums(nom,id,artiste,pistes,sortie,couverture) VALUES ('Hypnotize',1,1,12,2005,'https://upload.wikimedia.org/wikipedia/en/9/9a/System_Of_A_Down-Hypnotize.jpg');
+INSERT INTO albums(nom,id,artiste,pistes,sortie,couverture) VALUES ('System Of A Down',2,1,13,1998,'https://upload.wikimedia.org/wikipedia/en/b/bc/System_of_a_down.jpg');
+INSERT INTO albums(nom,id,artiste,pistes,sortie,couverture) VALUES ('Steal this album!',3,1,16,2002,'https://upload.wikimedia.org/wikipedia/en/4/45/StealThisAlbum.png');
+INSERT INTO albums(nom,id,artiste,pistes,sortie,couverture) VALUES ('Muscle Museum',4,3,7,1999,'https://upload.wikimedia.org/wikipedia/en/2/22/Musclemuseumep.jpg');
+INSERT INTO albums(nom,id,artiste,pistes,sortie,couverture) VALUES ('Absolution',5,3,14,2003,'https://upload.wikimedia.org/wikipedia/en/b/b4/Muse_-_Absolution_Cover_UK.jpg');
+INSERT INTO albums(nom,id,artiste,pistes,sortie,couverture) VALUES ('Nightmare Anatomy',6,4,11,2005,'https://upload.wikimedia.org/wikipedia/en/4/4f/Nightmare_Anatomy_Cover.jpg');
+INSERT INTO albums(nom,id,artiste,pistes,sortie,couverture) VALUES ('Knives',7,4,10,2009,'https://upload.wikimedia.org/wikipedia/en/9/9d/AidenKNIVES.jpg');
+INSERT INTO albums(nom,id,artiste,pistes,sortie,couverture) VALUES ('Sgt. Pepper''s Lonely Hearts Club Band',8,5,30,1968,'https://upload.wikimedia.org/wikipedia/en/5/50/Sgt._Pepper%27s_Lonely_Hearts_Club_Band.jpg');
+INSERT INTO albums(nom,id,artiste,pistes,sortie,couverture) VALUES ('The Beatles AKA the white album',9,5,12,1967,'https://upload.wikimedia.org/wikipedia/commons/2/20/TheBeatles68LP.jpg');
