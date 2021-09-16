f01540f3de86903c431db2149130b5cf4d2c1a9d
titouan.fuchs <titouan.fuchs@uha.fr>
2021-09-16 19:49:26 +0000

    Add groupes sql

---
+++ sql/groupes.sql
@@ -0,0 +1,16 @@
+CREATE TABLE albums(
+   nom      VARCHAR(30)
+  ,id       INTEGER 
+  ,chanteur VARCHAR(30)
+  ,origin   VARCHAR(30)
+  ,genre/0  VARCHAR(30)
+  ,genre/1  VARCHAR(30)
+  ,genre/2  VARCHAR(30)
+  ,genre/3  VARCHAR(30)
+);
+INSERT INTO albums(nom,id,chanteur,origin,genre/0,genre/1,genre/2,genre/3) VALUES ('System Of A Down',1,'Serj Tankian','Glendale','nu metal','hard rock','heavy metal','alternative metal');
+INSERT INTO albums(nom,id,chanteur,origin,genre/0,genre/1,genre/2,genre/3) VALUES ('Mika',2,'Michael Holbrook','Beyrouth','pop','pop-rock','classique','');
+INSERT INTO albums(nom,id,chanteur,origin,genre/0,genre/1,genre/2,genre/3) VALUES ('Muse',3,'Matthew Bellamy','Royaume-Uni','rock alternatif','rock progressif','new prog','art rock');
+INSERT INTO albums(nom,id,chanteur,origin,genre/0,genre/1,genre/2,genre/3) VALUES ('aiden',4,'William Francis','Seattle','Horror punk','post-hardcore','gothic rock','screamo');
+INSERT INTO albums(nom,id,chanteur,origin,genre/0,genre/1,genre/2,genre/3) VALUES ('The Beatles',5,'John Lennon','Liverpool','Rock','pop','psychedelique','');
+INSERT INTO albums(nom,id,chanteur,origin,genre/0,genre/1,genre/2,genre/3) VALUES ('Booba',6,'Elie Yaffa','Boulogne-Billancourt','hip-hop','rap francais','trap','gansta rap');
