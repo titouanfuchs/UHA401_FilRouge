2ac64f1c9cbc0a0b050a588a4dd8d2571689004e
titouan.fuchs <titouan.fuchs@uha.fr>
2021-09-19 08:51:28 +0200

    changement des infos de connexion à la bdd

--- connexion_base.php
+++ connexion_base.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    $_SESSION['user'] = 'musicpass';
-    $_SESSION['pass'] = 'UqnTy9qK72qiMen1';
+    $_SESSION['user'] = 'root';
+    $_SESSION['pass'] = '';
 
     global $bdd;
     $bdd = new PDO('mysql:host=localhost;dbname=musicpass;charset=utf8', "{$_SESSION['user']}", "{$_SESSION['pass']}");
