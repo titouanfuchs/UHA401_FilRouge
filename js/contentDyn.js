072398ba91412c41d0ea3482a1cb4e29d924cc1d
Doomiprane <38530673+HaagenProd@users.noreply.github.com>
2021-09-26 17:08:10 +0200

    Debut de la dynamisation avec le js

--- js/contentDyn.js
+++ js/contentDyn.js
@@ -1,18 +1,63 @@
-const groupCard = "<div class='groupCard'>\n" +
-    "\t<section class='groupCard-Img-Section'>\n" +
-    "\t\t<img class='albumCard-Img' src='images/NoCover.png'/>\n" +
-    "\t</section>        \n" +
-    "\t<section class='groupCard-Info-Section'>\n" +
-    "\t\t<h2>$groupName</h2>\n" +
-    "\t\t<h3>Chanteur : $groupChanteur</h3>\n" +
-    "\t\t<h3>Origine : $groupOrigin</h3>\n" +
-    "\t</section>\n" +
-    "\t<section class='groupCard-Genre-Section'>\n" +
-    "</div>";
-
 const groupContent = document.getElementById("groupContent");
 
-function addGroupCard(){
-    groupContent.innerHTML += groupCard;
-    console.log("test");
+document.onload = getGroupSearch();
+
+function getGroupSearch(arg){
+    groupContent.innerHTML = "";
+
+    if (arg == null){ //Quand pas de recherche effectuée;
+        addGroupCard()
+    }else{ //Quand une recherche est effectuée;
+        addGroupCard();
+    }
+}
+
+function addGroupCard(nom, chanteur, origine, genres){
+    //Creation de la nouvelle Carte
+    const newCard = document.createElement("div");
+    newCard.classList.add("groupCard");
+
+    //Section IMG
+    const IMGSection = document.createElement("section");
+    IMGSection.classList.add("groupCard-Img-Section");
+
+    const IMG = document.createElement("img");
+    IMG.classList.add("albumCard-Img");
+    IMG.setAttribute("src", "images/NoCover.png");
+
+    IMGSection.appendChild(IMG);
+
+    //Section Info
+    const InfoSection = document.createElement("section");
+    InfoSection.classList.add("groupCard-Info-Section");
+
+    const groupName = document.createElement("h2");
+    groupName.innerText += nom;
+
+    const groupChanteur = document.createElement("h3");
+    groupChanteur.innerText += "Chanteur : " + chanteur;
+
+    const groupOrigin = document.createElement("h3");
+    groupOrigin.innerText += "Origine : " + origine;
+
+    InfoSection.appendChild(groupName);
+    InfoSection.appendChild(groupChanteur);
+    InfoSection.appendChild(groupOrigin);
+
+    //Genre Section
+    const GenreSection = document.createElement("section");
+    GenreSection.classList.add("groupCard-Genre-Section");
+
+    for(let genre in genres){
+        let genreCase = document.createElement("button");
+        genreCase.innerText += genre;
+
+        GenreSection.appendChild(genreCase);
+    }
+
+    newCard.appendChild(IMGSection);
+    newCard.appendChild(InfoSection);
+    newCard.appendChild(GenreSection);
+
+    groupContent.appendChild(newCard);
 }
